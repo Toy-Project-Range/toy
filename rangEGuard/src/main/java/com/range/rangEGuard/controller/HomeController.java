5000616ab5ad13ffcde1190c0a6fd6b947b5a710
hyun <nomzaxs@gmail.com>
2022-02-26 14:34:52 +0900

    modelandview

--- rangEGuard/src/main/java/com/range/rangEGuard/controller/HomeController.java
+++ rangEGuard/src/main/java/com/range/rangEGuard/controller/HomeController.java
@@ -1,15 +1,10 @@
 package com.range.rangEGuard.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -22,18 +17,33 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	@RequestMapping("/home.do")
+	public ModelAndView home() {
+		ModelAndView mav = new ModelAndView();
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		mav.addObject("serverContry", "Korea");
+		mav.setViewName("home");
 		
-		String formattedDate = dateFormat.format(date);
+		return mav;
+	}
+	
+	@RequestMapping("/login.do")
+	public ModelAndView login() {
+		ModelAndView mav = new ModelAndView();
 		
-		model.addAttribute("serverTime", formattedDate );
+		mav.addObject("data", "login");
+		mav.setViewName("login");
 		
-		return "login";
+		return mav;
+	}
+	
+	@RequestMapping("/main.do")
+	public ModelAndView main() {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("main");
+		
+		return mav;
 	}
 	
 }
