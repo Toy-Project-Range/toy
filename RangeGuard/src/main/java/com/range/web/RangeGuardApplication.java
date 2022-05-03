1af47cffa915579bf95523f9735f307629539dea
nomzaxs <nomzaxs@gmail.com>
2022-05-03 22:11:26 +0900

    make project spring boot

---
+++ RangeGuard/src/main/java/com/range/web/RangeGuardApplication.java
@@ -0,0 +1,13 @@
+package com.range.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangeGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RangeGuardApplication.class, args);
+	}
+
+}
---
+++ RangeGuard/src/test/java/com/range/web/RangeGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.range.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangeGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
